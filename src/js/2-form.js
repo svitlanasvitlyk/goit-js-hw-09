2e9e884b2cecc0008f0d4adf54c89d81ec26e1e2
svitlanasvitlyk <65176415+svitlanasvitlyk@users.noreply.github.com>
2024-11-06 21:05:55 +0100

    idk

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const form = document.querySelector('.feedback-form');
 const input = document.querySelector('input');
 const btn = document.querySelector('button');
@@ -14,6 +15,24 @@ input.addEventListener('focus', () => {
 });
 input.addEventListener('blur', () => {
   input.placeholder = '';
+=======
+const form = document.querySelector(".feedback-form");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+const textInput = document.querySelector("textarea");
+
+const localStorageKey = "feedback-form-state";
+
+const savedData = JSON.parse(localStorage.getItem(localStorageKey)) || {};
+input.value = savedData.email || "";
+textInput.value = savedData.message || "";
+
+input.addEventListener("focus", () => {
+  input.placeholder = "Type area";
+});
+input.addEventListener("blur", () => {
+  input.placeholder = "";
+>>>>>>> Stashed changes
 });
 
 function saveForm() {
@@ -24,17 +43,29 @@ function saveForm() {
   localStorage.setItem(localStorageKey, JSON.stringify(formData));
 }
 
+<<<<<<< Updated upstream
 input.addEventListener('input', saveForm);
 textInput.addEventListener('input', saveForm);
 
 form.addEventListener('submit', submitBtn);
+=======
+input.addEventListener("input", saveForm);
+textInput.addEventListener("input", saveForm);
+
+form.addEventListener("submit", submitBtn);
+>>>>>>> Stashed changes
 
 function submitBtn(event) {
   event.preventDefault();
   const email = input.value.trim();
   const message = textInput.value.trim();
+<<<<<<< Updated upstream
   if (email === '' || message === '') {
     return alert('Email field must be filled in');
+=======
+  if (email === "" || message === "") {
+    return alert("Email field must be filled in");
+>>>>>>> Stashed changes
   }
 
   const userData = {
